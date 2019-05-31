c1a9caa44bef7a69fc0e96a4c9f9f43822a64d24
Diego Cabo <dcgdiego@gmail.com>
2019-05-31 16:19:36 +0200

    Changes so we have a nice title without log formatting.

--- run.py
+++ run.py
@@ -2,13 +2,31 @@ import logging
 
 from Game import Game
 
-# delete filemode = 'w' to append log to previous run
-# with this format='%(message)s' to write messages without the INFO:root:
-# logging.basicConfig(filename='output.log', filemode='w', level=logging.DEBUG, format='%(message)s')
+"""Print nice Logo and Title"""
+f = open("output.log", 'w')
+title = """Simulation of simplified version of the well known Kwartet game.
 
+          _____
+         |A .  | _____
+         | /.\ ||A ^  | _____
+         |(_._)|| / \ ||A _  | _____
+         |  |  || \ / || ( ) ||A_ _ |
+         |____V||  .  ||(_'_)||( v )|
+                |____V||  |  || \ / |
+                       |____V||  .  |
+                              |____V|
+
+
+Done by Diego Cabo, Tanja de Vries and Ruben Kip\n"""
+f.write(title)
+f.write("\n---------------------------------------------------------------\n\n")
+f.close()
+
+# delete filemode = 'w' to append log to previous run
+# with this format='%(message)s' to write messages without the <time> INFO:root:
+# logging.basicConfig(filename='output.log', filemode='w', level=logging.DEBUG, format='%(message)s')
 logging.basicConfig(filename='output.log',
                     level=logging.DEBUG,
-                    filemode='w',
                     format='%(asctime)s -  %(levelname)s - %(message)s')
 
 
@@ -19,18 +37,7 @@ formatter = logging.Formatter('%(levelname)s - %(message)s')
 console.setFormatter(formatter)
 logging.getLogger().addHandler(console)
 logging.info("Starting simulation")
-icon = """
-          _____
-         |A .  | _____
-         | /.\ ||A ^  | _____
-         |(_._)|| / \ ||A _  | _____
-         |  |  || \ / || ( ) ||A_ _ |
-         |____V||  .  ||(_'_)||( v )|
-                |____V||  |  || \ / |
-                       |____V||  .  |
-                              |____V|
-"""
-logging.info(icon)
+
 game = Game()
 game.initGame()
 game.startGame()
